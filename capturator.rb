1d540699d870133331f53191b5a4a8ca80580d64
Urpo Lankinen <wwwwolf@iki.fi>
2005-07-19 14:15:28 +0000

    Added the object-oriented stuff to the actual capturator.

--- capturator.rb
+++ capturator.rb
@@ -6,6 +6,8 @@
 # For managing those #%&@!ng mencoder tv-capture command strings that no
 # sane person can ever fully memorise.
 #
+# "Object orientation done fruitily."
+#
 # (c) Urpo "WWWWolf" Lankinen 2005
 # You can use, modify and distribute this thing without any restrictions,
 # just leave this copyright notice here as it is. There is no warranty
@@ -15,22 +17,209 @@
 
 require 'optparse'
 
-MencoderBinary = '/usr/bin/mencoder'
-# Describe your video and audio profiles here
-VideoProfiles = {
-  'BigMPEG4' => "FFMPEG MPEG4 @ 1800kbps, 720x544",
-  'BigMJPEG' => "MJPEG, 720x544",
-  'BigHuffYUV' => "HuffYUV, 720x544",
-  'NetXviD' => "XviD @ 900kbps, 352x288",
-  'BigXviD' => "XviD @ 1800kbps, 720x544",
-}
-AudioProfiles = {
-  'MP3-64kCBR' => "LAME MP3, 64kbps constant bitrate"
-}
+###########################################################################
+# Global settings
 
-outputfile = 'output.avi'
-videoprofile = 'BigMPEG4'
-audioprofile = 'MP3-64kCBR'
+MencoderBinary = '/usr/bin/mencoder'
+DefaultVideoFormat = 'yuy2'
+DefaultOutputFile = 'output.avi'
+DefaultVideoProfile = 'BigMPEG4'
+DefaultAudioProfile = 'MP364kCBRAudio'
+
+###########################################################################
+# Some class definitions here.
+
+class Profile
+  attr :description, false
+  attr :codec, false
+  attr :options, false
+
+  def codec_option_switch
+    fail "Can't do."
+  end
+  def paramstring
+    ret = ''
+    @options.keys.each do |o|
+      ret = ret + o +
+	(@options[o].nil? ? "" : "=" + @options[o]) + ':'
+    end
+    ret.chop!
+  end
+  def options
+    return [codec_option_switch, paramstring]
+  end
+
+  def Profile.profiles_like_this
+    if self.superclass != Profile
+      return []
+    end
+    l = []
+    ObjectSpace.each_object(Class) do |c|
+      if c.superclass == self
+	l.push(c)
+      end
+    end
+    return l
+  end
+
+end
+
+class VideoProfile < Profile
+  attr :filters, false
+  attr :videoformat, false
+
+  class VideoFilters
+    attr :filters, false
+    def initialize
+      @filters = [
+	[ 'crop', '720:544:24:16' ],
+	[ 'pp', 'lb' ]
+      ]
+    end
+    def add(filter, parms)
+      @filters.push([filter,parms])
+    end
+    def paramstring
+      ret = ''
+      @filters.each do |f|
+	if f.length != 2 then
+	  fail "incorrectly specified filter filter #{f.inspect}"
+	end
+	ret = ret + f[0] + 
+	  (f[1].nil? ? "" : "=" + f[1]) + ","
+      end
+      ret.chop!
+    end
+  end
+
+  def initialize
+    @filters = VideoFilters.new
+    @videoformat = DefaultVideoFormat
+  end
+
+  def codec_option_switch
+    case @codec
+      when 'lavc' then '-lavcopts'
+      when 'xvid' then '-xvidencopts'
+    else fail "Unknown codec #{@codec}"
+    end
+  end
+end
+
+class AudioProfile < Profile
+  def codec_option_switch
+    case @codec
+      when 'mp3lame' then '-lameopts'
+    else fail "Unknown codec #{@codec}"
+    end
+  end
+end
+class TVProfile < Profile
+end
+
+def listprofs
+  def listp(ct)
+    ct.profiles_like_this.each do |c|
+      a = c.new
+      name = c.name
+      desc = a.description
+      printf("%-15s %s\n",c.name,a.description)
+    end
+  end
+  puts "\nVideo profiles:"
+  listp(VideoProfile)
+  puts "\nAudio profiles:"
+  listp(AudioProfile)
+  puts
+  exit
+end
+
+###########################################################################
+# Video profiles
+
+class BigMPEG4 < VideoProfile
+  def initialize
+    super()
+    @description = "FFMPEG MPEG4 @ 1800kbps, 720x544"
+    @codec = 'lavc'
+    @options = {
+      'vcodec' => 'mpeg4',
+      'vbitrate' => '1800',
+      'vme' => '0',
+      'keyint' => '250'
+    }
+  end
+end
+class BigMJPEG < VideoProfile
+  def initialize
+    super()
+    @description = "MJPEG, 720x544"
+    @codec = 'lavc'
+    @options = {
+      'vcodec' => 'mjpeg',
+      #'mbd' => '1',
+      #'vbitrate' => '1800'
+    }
+  end
+end
+class BigHuffYUV < VideoProfile
+  def initialize
+    super()
+    @description = "HuffYUV, 720x544"
+    @codec = 'lavc'
+    @options = {
+      'vcodec' => 'huffyuv'
+    }
+  end
+end
+class NetXviD < VideoProfile
+  def initialize
+    super()
+    @description = "XviD @ 900kbps, 352x288"
+    @codec = 'xvid'
+    @options = {
+      'bitrate' => '900',
+    }
+    @filters.add('scale', '384:288')
+  end
+  # swscaler = '1'
+end
+class BigXviD < VideoProfile
+  def initialize
+    super()
+    @description = "XviD @ 1800kbps, 720x544"
+    @codec = 'xvid'
+    @options = {
+      'bitrate' => '1800',
+      #'quant_type' => 'mpeg'
+    }
+    #videoformat = nil
+    #tvoptions.delete('outfmt')
+  end
+end
+
+# Audio profiles
+
+class MP364kCBRAudio < AudioProfile
+  def initialize
+    super()  
+    @description = "LAME MP3, 64kbps constant bitrate"
+    @codec = 'mp3lame'
+    @options = {
+      'cbr' => nil,
+      'br' => '64'
+    }
+  end
+end
+
+
+###########################################################################
+# Process the parameters
+
+outputfile = DefaultOutputFile
+videoprofile = DefaultVideoProfile
+audioprofile = DefaultAudioProfile
+videoformat = DefaultVideoFormat
 justparms = false
 
 ARGV.options do |opts|
@@ -39,165 +228,74 @@ ARGV.options do |opts|
 	  "Default: output.avi") { |outputfile| }
   opts.on("-v", "--videoprofile=profile", String,
 	  "Which set of video settings to use.",
-	  "Default: BigMPEG4",
-	  "Available: " + VideoProfiles.keys.join(" ")) { |videoprofile| }
+	  "Default: " + DefaultVideoProfile,
+	  "Available: " + VideoProfile.profiles_like_this.join(" ")) do 
+    |videoprofile|
+  end
   opts.on("-a", "--audioprofile=profile", String,
 	  "Which set of audio settings to use.",
-	  "Default: MP3-64kCBR",
-	  "Available: " + AudioProfiles.keys.join(" ")) { |audioprofile| }
+	  "Default: " + DefaultAudioProfile,
+	  "Available: " + AudioProfile.profiles_like_this.join(" ")) do
+    |audioprofile|
+  end
   opts.on("-p", "--show-parameters",
 	  "Show which mencoder parameters are being used.") {justparms = true}
   opts.on("-l", "--describe-profiles",
-          "More detailed information on each of the profiles.") do
-    puts "\nVideo profiles:"
-    VideoProfiles.keys.each { |p| puts "\t#{p}\t\t#{VideoProfiles[p]}" }
-    puts "\nAudio profiles:"
-    AudioProfiles.keys.each { |p| puts "\t#{p}\t\t#{AudioProfiles[p]}" }
-    puts
-    exit
-  end
+          "More detailed information on each of the profiles.") { listprofs }
   opts.on("-h", "--help",
 	  "Shows this help message.") { puts opts; exit }
   opts.parse!
 end
 
 ###########################################################################
+# Get our profiles.
 
-def optstostring(opts)
-  ret = ''
-  opts.keys.each do |o|
-    ret = ret + o +
-      (opts[o].nil? ? "" : "=" + opts[o]) + ':'
-  end
-  ret.chop!
-  return ret
-end
-
-def filterstostring(flts)
-  ret = ''
-  flts.each do |f|
-    if f.length != 2 then
-      fail "incorrectly specified filter filter #{f.inspect}"
-    end
-    ret = ret + f[0] + 
-      (f[1].nil? ? "" : "=" + f[1]) + ","
-  end
-  ret.chop!
-  return ret
-end
+vprof = eval("#{videoprofile}.new")
+aprof = eval("#{audioprofile}.new")
 
 ###########################################################################
+# TV settings
 
-videocodec = nil
-audiocodec = nil
-videooptions = nil
-audiooptions = nil
-videoformat = 'yuy2'
-videoinformat = nil   # Set this to make -vc use different raw format than -tv.
-                      # Setting to 'obey' doesn't use -vc if videoformat
-                      # is specified.
-swscaler = nil
-
-VideoCodecOption = {
-  'lavc' => '-lavcopts',
-  'xvid' => '-xvidencopts'
-}
-AudioCodecOption = {
-  'mp3lame' => '-lameopts'
-}
-
-###########################################################################
-
-# Set up your television settings here.
-tvoptions = {
-  'driver' => 'v4l2',
-  'width' => '768',
-  'height' => '576',
-  'outfmt' => videoformat,
-  'fps' => '25',
-  'alsa' => nil
-}
-
-# These are the filters that are applied to all profiles.
-videofilters = [
-  [ 'crop', '720:544:24:16' ],
-  [ 'pp', 'lb' ]
-]
-
-# The video profiles.
-case videoprofile
-when 'BigMPEG4' then
-  videocodec = 'lavc'
-  videooptions = {
-    'vcodec' => 'mpeg4',
-    'vbitrate' => '1800',
-    'vme' => '0',
-    'keyint' => '250'
-  }
-when 'BigMJPEG' then
-  videocodec = 'lavc'
-  videooptions = {
-    'vcodec' => 'mjpeg',
-    #'mbd' => '1',
-    #'vbitrate' => '1800'
-  }
-when 'BigHuffYUV' then
-  videocodec = 'lavc'
-  videooptions = {
-    'vcodec' => 'huffyuv'
-  }
-when 'NetXviD' then
-  videocodec = 'xvid'
-  videooptions = {
-    'bitrate' => '900',
-  }
-  videofilters.push(['scale','384:288'])
-  swscaler = '1'
-when 'BigXviD' then
-  videocodec = 'xvid'
-  videooptions = {
-    'bitrate' => '1800',
-    #'quant_type' => 'mpeg'
-  }
-  #videoformat = nil
-  #tvoptions.delete('outfmt')
+class GlobalTVOptions < TVProfile
+  def initialize
+    super()
+    @options = {
+      'driver' => 'v4l2',
+      'width' => '768',
+      'height' => '576',
+      'outfmt' => DefaultVideoFormat,
+      'fps' => '25',
+      'alsa' => nil
+    }
+  end
 end
-
-# THe audio profiles.
-case audioprofile
-when 'MP3-64kCBR' then
-  audiocodec = 'mp3lame'
-  audiooptions = {
-    'cbr' => nil,
-    'br' => '64'
-  }
-end
+tvoptions = GlobalTVOptions.new
 
 ###########################################################################
 
 # Let's build the option string...
 mencoderopts = [
   'tv://',
-  '-tv', optstostring(tvoptions),
+  '-tv', tvoptions.paramstring,
   '-of', 'avi',
-  '-ovc', videocodec,
-  VideoCodecOption[videocodec], optstostring(videooptions),
-  '-oac', audiocodec,
-  AudioCodecOption[audiocodec], optstostring(audiooptions),
-  '-vf',  filterstostring(videofilters),
-  '-o', outputfile
-]
-if not swscaler.nil?
-  mencoderopts.push('-sws', swscaler)
-end
+  '-ovc', vprof.codec]
+mencoderopts.push(*vprof.options)
+mencoderopts.push('-oac', aprof.codec)
+mencoderopts.push(*aprof.options)
+mencoderopts.push('-vf', vprof.filters.paramstring,
+		  '-o', outputfile)
 
-if (not videoformat.nil?) and videoinformat != 'obey'
-  if videoinformat.nil?
-    mencoderopts.push('-vc','raw' + videoformat)
-  else
-    mencoderopts.push('-vc','raw' + videoinformat)
-  end
-end
+#if not swscaler.nil?
+#  mencoderopts.push('-sws', swscaler)
+#end
+
+#if (not videoformat.nil?) and videoinformat != 'obey'
+#  if videoinformat.nil?
+#    mencoderopts.push('-vc','raw' + videoformat)
+#  else
+#    mencoderopts.push('-vc','raw' + videoinformat)
+#  end
+#end
 
 # And here is what we finally do.
 if justparms
