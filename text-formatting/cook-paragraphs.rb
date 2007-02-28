d1292200471888c1e7ddca3dd10f76fc517ae7af
Urpo Lankinen <wwwwolf@iki.fi>
2007-02-28 12:42:54 +0000

    Here's a random crappy script to do Stuff.

---
+++ text-formatting/cook-paragraphs.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+# $Id$
+# I've probably written a script like this many times. This is one of them.
+
+str = ""
+
+while(a = STDIN.gets) do
+	a.chomp!
+	if(a != "") then
+		str += " " + a.chomp;
+	else
+		puts "\n"
+		puts str[1..-1]
+		str = ""
+	end
+end
+
+puts "\n"
+puts str[1..-1]
