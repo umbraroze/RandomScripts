c672123e9d255ce8d8f59bbec84ad1c2219aa52e
Urpo Lankinen <wwwwolf@iki.fi>
2007-05-15 09:50:34 +0000

    IPv6 considerations.

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -11,24 +11,29 @@ ip6tables -F
 ip6tables -X
 ip6tables -Z
 # Set policies.
-iptables -P INPUT ACCEPT
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
-ip6tables -P INPUT DROP
+iptables  -P INPUT ACCEPT
+ip6tables -P INPUT ACCEPT
+iptables  -P FORWARD DROP
 ip6tables -P FORWARD DROP
+iptables  -P OUTPUT ACCEPT
 ip6tables -P OUTPUT ACCEPT
 
 # Don't allow anyone to connect to these ports from outside.
 # smtp
 iptables -A INPUT -p tcp -i $NETIF -d $MYIP --dport 25 -j REJECT
+ip6tables -A INPUT -p tcp -i $NETIF --dport 25 -j REJECT
 # ssh
 iptables -A INPUT -p tcp -i $NETIF -d $MYIP --dport 2209 -j REJECT
+ip6tables -A INPUT -p tcp -i $NETIF --dport 2209 -j REJECT
 # mysql
 iptables -A INPUT -p tcp -i $NETIF -d $MYIP --dport 3306 -j REJECT
+ip6tables -A INPUT -p tcp -i $NETIF --dport 3306 -j REJECT
 # postgresql
 iptables -A INPUT -p tcp -i $NETIF -d $MYIP --dport 5432 -j REJECT
+ip6tables -A INPUT -p tcp -i $NETIF --dport 5432 -j REJECT
 # X11 just in case
 iptables -A INPUT -p tcp -i $NETIF -d $MYIP --dport 6000:6100 -j REJECT
+ip6tables -A INPUT -p tcp -i $NETIF --dport 6000:6100 -j REJECT
 
 # And on IPv6, we *can* talk to HTTP.
-ip6tables -A INPUT -p tcp -i $NETIF --dport 80 -j ACCEPT
+#ip6tables -A INPUT -p tcp -i $NETIF --dport 80 -j ACCEPT
