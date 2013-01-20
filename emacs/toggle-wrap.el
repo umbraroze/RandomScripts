15de43c45a76734926b2d70080628148e1392122
Urpo Lankinen <wwwwolf@iki.fi>
2013-01-20 16:34:49 +0200

    Emacs stuff has been moved to a repostitory of its own (github: wwwwolf/emacs-hacks)

--- emacs/toggle-wrap.el
+++
@@ -1,7 +0,0 @@
-(defun toggle-wrap-modes ()
-  "Toggles between auto-fill and visual-line modes. The command assumes only
-one is active at the time."
-  (interactive)
-  (auto-fill-mode)
-  (visual-line-mode))
-(provide 'toggle-wrap)
