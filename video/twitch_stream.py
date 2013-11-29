f3099dbacadc8c13d59a9e46c18497c002594966
Urpo Lankinen <wwwwolf@iki.fi>
2013-11-29 20:05:59 +0200

    Widescreen support and such garbage

--- video/twitch_stream.py
+++ video/twitch_stream.py
@@ -17,6 +17,10 @@
 #   twitch_stream.py --file=output.flv  - Captures to file. Useful for
 #                                         previewing stream quality.
 #
+# Additional options:
+#   --widescreen                        - Stream in 16:9. If unspecified,
+#                                         use 4:3.
+#
 #######################################################################
 #
 # (c) Urpo Lankinen 2013. I give permission to use this script for any
@@ -26,7 +30,6 @@
 #######################################################################
 
 settings_file = '~/.twitch_stream_settings'
-ffmpeg_binary = '/usr/bin/ffmpeg'
 twitch_rtmp = 'rtmp://live.twitch.tv/app/%s'
 
 #######################################################################
@@ -34,48 +37,84 @@ twitch_rtmp = 'rtmp://live.twitch.tv/app/%s'
 import os
 import sys
 import getopt
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElementTree
 
-tree = ET.parse(os.path.expanduser(settings_file))
-root = tree.getroot()
+class param:
+    def __init__(this,settings):
+        this._tree = ElementTree.parse(os.path.expanduser(settings))
+        this._root = this._tree.getroot()
+    def __getitem__(this,n):
+        t = None
+        try:
+            a = this._root.findall('./%s' % n)
+            t = a[0].text
+        except IndexError:
+            t = None
+        return t
 
-output = twitch_rtmp % root.findall('./endpoint/key')[0].text
+param = param(settings_file)
 
-opts, args = getopt.gnu_getopt(sys.argv[1:], '', ['file=','dryrun'])
+widescreen = False
+output = twitch_rtmp % param['endpoint/key']
+
+opts, args = getopt.gnu_getopt(sys.argv[1:], '',
+                               ['file=','dryrun','widescreen'])
 for o in opts:
     if '--file' in o:
         output = o[1]
     if '--dryrun' in o:
         output = '/dev/null' # should probably be crossplatform or something?
+    if '--widescreen' in o:
+        widescreen = True
 
+ffmpeg_binary = None
+if os.path.exists('/usr/bin/ffmpeg'):
+    ffmpeg_binary = '/usr/bin/ffmpeg'
+elif os.path.exists('/usr/bin/avconv'):
+    ffmpeg_binary = '/usr/bin/avconv'
+else:
+    ffmpeg_binary = param['encoder']
+if ffmpeg_binary == None:
+    print("Can't find either ffmpeg or avconv.")
+    sys.exit(1)
+
+aspect = '4:3'
+if widescreen:
+    aspect = '16:9'
+size = param['video/size']
+if widescreen:
+    size = param['video/sizewidescreen']
 ffmpeg_params = [
     ffmpeg_binary,
     # Video device.
     '-f', 'video4linux2',
-    '-i', root.findall('./video/device')[0].text,
+    '-i', param['video/device'],
     # General video settings
-    '-r', root.findall('./video/framerate')[0].text,
-    '-s', root.findall('./video/size')[0].text,
+    '-r', param['video/framerate'],
+    '-s', size,
+    '-aspect', aspect,
     # Video encoder. We set b:vr=minrate=maxrate to approximate CBR
     '-c:v', 'libx264',
-    '-g', root.findall('./video/keyframerate')[0].text,
-    '-b:v', root.findall('./video/bitrate')[0].text,
-    '-minrate', root.findall('./video/bitrate')[0].text,
-    '-maxrate', root.findall('./video/bitrate')[0].text,
-    '-bufsize:0', root.findall('./video/buffer')[0].text,
-    '-preset', root.findall('./video/preset')[0].text,
+    '-g', param['video/keyframerate'],
+    '-b:v', param['video/bitrate'],
+    '-minrate', param['video/bitrate'],
+    '-maxrate', param['video/bitrate'],
+    '-bufsize:0', param['video/buffer'],
+    '-preset', param['video/preset'],
     # Audio input
     '-f', 'alsa',
-    '-i', root.findall('./audio/device')[0].text,
+    '-i', param['audio/device'],
     '-ar', '44100',
     '-c:a', 'libmp3lame',
-    '-b:a', root.findall('./audio/bitrate')[0].text,
-    '-bufsize:1', root.findall('./audio/buffer')[0].text,
+    '-b:a', param['audio/bitrate'],
+    '-bufsize:1', param['audio/buffer'],
     # Output stream
     '-f', 'flv',
     '-y',
     output
 ]
 
-os.execv(ffmpeg_binary,ffmpeg_params)
+print(ffmpeg_params)
+#os.execv(ffmpeg_binary,ffmpeg_params)
+
 
