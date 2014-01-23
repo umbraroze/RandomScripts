bc1ccd8b85e4ba9162f820a0967719fc02cb3edd
Urpo Lankinen <wwwwolf@iki.fi>
2014-01-23 15:39:00 +0200

    Some test cap shit

--- video/testcap.sh
+++ video/testcap.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# 427x240
-# 854x480
+# RAW capture resolution: 720x480
+# 16:9 upscaled: 854x480
+# 16:9 half-quality: 427x240
+# 4:3 low quality: 360x240
 
 /usr/bin/ffmpeg \
    -f video4linux2 -r 25 -i /dev/video1 \
-   -f alsa -i hw:0 \
+   -f alsa -i hw:1 \
    -f mp4 \
-   -pix_fmt yuv422p \
+   -pix_fmt yuv422p -filter:v 'yadif,scale=360:240' \
    -c:v libx264 -b:v 1200k -g 50 -preset ultrafast \
    -c:a libmp3lame -b:a 64k -bufsize:1 16000k \
    -y /tmp/test.mp4
