71bfedc5ac294dd7783458308f119708c7dfaf67
Urpo Lankinen <wwwwolf@iki.fi>
2018-06-19 21:51:12 +0300

    OBS capture auto-remuxer added

---
+++ video/remux_obs_captures.ps1
@@ -0,0 +1,22 @@
+﻿############################################################
+# Take all .flv files in current directory, run them through
+# ffmpeg in copy stream mode to convert them to mp4.
+# Will make the files better behaved in playback and
+# editing (for example, vlc will refuse to seek the flv
+# files for no reason at all).
+############################################################
+
+## FFmpeg location
+#$ffmpeg = $home + "\Applications\ffmpeg\bin\ffmpeg.exe";
+$ffmpeg = $env:ProgramFiles + "\FFmpeg\ffmpeg\bin\ffmpeg.exe";
+
+## The meat
+
+$infiles = Get-Item *.flv;
+$infiles | ForEach-Object {
+    $in = $_;
+    $p = Split-Path $in;
+    $out = $p  + "\" + $in.BaseName + ".mp4";
+    echo "`n`nProcessing $in => $out`n`n";
+    & $ffmpeg -i $in -c:v copy -c:a copy $out
+}
