e1eb96979fecee3d87a15c13a53197652d9a23e0
Rose Midford <wwwwolf@iki.fi>
2022-02-25 14:18:36 +0200

    Added "fix memorycard recovery" script

---
+++ photo_arc/fix_memorycard_recovery.ps1
@@ -0,0 +1,17 @@
+# Postprocessing stuff from Recuva SD card recovery. Results in files
+# like "[000001].tif", with DNG files identified as TIFFs. Rename all
+# files to img000001.dng or .jpg as appropriate. Fix file timestamps
+# using exiftool.
+foreach ($item in (Get-Item *.tif)) {
+    $oldname = ($item.Name)
+    $newname = ("img" + $item.Basename + ".dng") -Replace '[\[\]]',''
+    Move-Item -LiteralPath $oldname -Destination $newname
+    Write-Output "${oldname} -> ${newname}"
+}
+foreach ($item in (Get-Item *.jpg)) {
+    $oldname = ($item.Name)
+    $newname = ("img" + $item.Name) -Replace '[\[\]]',''
+    Move-Item -LiteralPath $oldname -Destination $newname
+    Write-Output "${oldname} -> ${newname}"
+}
+& exiftool.exe '-FileModifyDate<DateTimeOriginal' *.dng *.jpg
